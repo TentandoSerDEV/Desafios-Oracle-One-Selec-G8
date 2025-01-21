d68b130f2f2b8229903d8f39bceb7cdcf6f64b59
Renan de Almeida <101058122+TentandoSerDEV@users.noreply.github.com>
2025-01-21 17:22:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+/*/contador = 1;
+//while (contador <=10) {
+//    alert(contador);
+//    contador++;
+//}
+
+contador = 10;
+while (contador >=0) {
+    alert(contador);
+    contador--;
+}
+
+numeroQtd = prompt("Digite um numero");
+numeroQtd = parseInt(numeroQtd);
+contador = numeroQtd;
+while (numeroQtd >=0) {
+    alert(contador);
+    contador--;
+}*/
+numeroQtd = prompt("Digite um numero");
+numeroQtd = parseInt(numeroQtd);
+contador = 0;
+while (contador <= numeroQtd) {
+    alert(contador);
+    contador++;
+}
